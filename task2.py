a7d4375e3494fcf35ebe30bc0fbbe6bf9ec1f161
Konstantin <pkv39konstantin@yandex.ru>
2022-12-20 00:03:14 +0300

    correct1

--- task2.py
+++ task2.py
@@ -3,6 +3,6 @@
 
 for x in range(2):
     for y in range(2):
-        for z in range(3):
+        for z in range(2):
             print ((not(x or y or z))== (not(x) and not(y) and not(z)), end='\t')
 

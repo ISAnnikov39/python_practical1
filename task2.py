361e0568050e03b7bd918cd5d4b65294dcbbdb00
Konstantin <pkv39konstantin@yandex.ru>
2022-12-19 23:59:01 +0300

    did task 2

---
+++ task2.py
@@ -0,0 +1,10 @@
+#Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+# x = int (input("Введите значение х "))
+# y = int (input("Введите значение y "))
+# z = int (input("Введите значение z "))
+for x in range(2):
+    for y in range(2):
+        for z in range(3):
+            print ((not(x or y or z))== (not(x) and not(y) and not(z)), end='\t')
+
